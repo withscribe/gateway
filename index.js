5f7df35426f6eb8384baa651dd0606c2a0d0abb0
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2018-09-04 18:33:57 -0400

    Reverted back to having resolvers implemented in mergeSchemas

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express'),
     app = express(),
-    PORT = process.env.PORT || 8082,
+    PORT = process.env.PORT || 8081,
     cors = require('cors'),
     { ApolloServer } = require('apollo-server-express'),
     { ApolloEngine } = require('apollo-engine'),
@@ -31,12 +31,118 @@ const path = '/unravel';
                 profileSchema,
                 authSchema,
                 linkTypeDefs
-            ], 
+            ],
+            resolvers: ({
+                Account: {
+                    profile: {
+                        fragment: `fragment AccountFragment on Account { id }`,
+                        resolve(parent, args, context, info) {
+                            return info.mergeInfo.delegate(
+                                'query',
+                                'profileByAccountId',
+                                {
+                                    accountId: parent.id
+                                },
+                                context,
+                                info
+            
+                            )
+                        },
+                    },
+                },
+                Profile: {
+                    account: {
+                        fragment: `fragment ProfileFragment on Profile { id }`,
+                        resolve(parent, args, context, info) {
+                            return info.mergeInfo.delegate(
+                                'query',
+                                'accountByProfileId',
+                                {
+                                    profileID: parent.id
+                                },
+                                context,
+                                info
+            
+                            )
+                        }
+                    },
+                    stories: {
+                        fragment: `fragment ProfileFragment on Profile { id }`,
+                        resolve(parent, args, context, info) {
+                            return info.mergeInfo.delegate(
+                                'query',
+                                'storiesByProfileId',
+                                {
+                                    profileId: parent.id
+                                },
+                                context,
+                                info
+                            )
+                        }
+            
+                    }
+                },
+                Mutation: {
+                   registerAccountWithProfile: {
+                    fragment: `fragment RegisterFragment on Account { id }`,
+                    resolve: async (parent, args, context, info) => {
+            
+                            // register the user
+                            const { account, token, refreshToken } = await info.mergeInfo.delegate(
+                                'mutation',
+                                'register',
+                                {
+                                    email: args.email,
+                                    password: args.password,
+            
+                                },
+                                context,
+                                info
+                            )
+                            console.log(account)
+            
+                            // register the user profile to the newly created account
+                            const profile = await info.mergeInfo.delegate(
+                                'mutation',
+                                'registerProfile',
+                                {
+                                    accountId: account.id,
+                                    userName: args.userName
+                                },
+                                context,
+                                info
+            
+                            )
+            
+                            console.log(profile)
+            
+                            // attach the profile to the new user account
+                            info.mergeInfo.delegate(
+                                'mutation',
+                                'setProfileToAccount',
+                                {
+                                    accountId: account.id,
+                                    profileID: profile.id
+                                },
+                                context,
+                                info
+                            )
+            
+                        return {
+                            token,
+                            refreshToken,
+                            account
+                        }
+                    }
+            
+                   },
+                }
+            }),
         });
 
         const server = new ApolloServer({
             schema: mergedSchema,
-            resolvers: linkResolvers,
+            //resolvers: linkResolvers,
             // tracing: true,
             // cacheControl: true,
             // // We set `engine` to false, so that the new agent is not used.
